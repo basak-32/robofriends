540255536dd7b5830a4c605d89d77f5797856594
basak-32 <ranajit1998basak@gmail.com>
2020-05-12 16:40:48 -0600

    adding robofriends app

---
+++ src/components/robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Lanne Graham',
+        username: 'Bret',
+        email: 'sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Sally Hom',
+        username: 'Huil',
+        email: 'lisa@home.tv'
+    },
+    {
+        id: 3,
+        name: 'Kelly John',
+        username: 'molly',
+        email: 'ham@snipper.mn'
+    },
+    {
+        id: 4,
+        name: 'Britain',
+        username: 'John',
+        email: 'jesus@kill.in'
+    },
+    {
+        id: 5,
+        name: 'Cat',
+        username: 'Kitty',
+        email: 'abc@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Dod',
+        username: 'Puppy',
+        email: 'puppy@dod.co'
+    },
+    {
+        id: 7,
+        name: 'Horse',
+        username: 'hett',
+        email: 'stable@in.cov'
+    },
+    {
+        id: 8,
+        name: 'Parrot',
+        username: 'lovely',
+        email: 'parrot@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Lion',
+        username: 'Hero',
+        email: 'den@yesenia.net'
+    },
+    {
+        id: 10,
+        name: 'Tiger',
+        username: 'King',
+        email: 'ceaser@kory.org'
+    },
+    {
+        id: 11,
+        name: 'Peacock',
+        username: 'beauty',
+        email: 'beauty@ful.com'
+    },
+    {
+        id: 12,
+        name: 'Monkey',
+        username: 'hamper',
+        email: 'cage@city.in'
+    }
+]
