df182a5b5c336f511441e6015eefca007544e525
basak-32 <ranajit1998basak@gmail.com>
2020-05-18 20:48:02 +0530

    Updates

---
+++ precache-manifest.2ffd1a80301352848871e371c3080792.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d481b3c4fa17e6720153d17afb62dbc",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "84ca485f6fb5f1978a9f",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "76f82c27bebbda439461",
+    "url": "/robofriends/static/css/main.dad2802e.chunk.css"
+  },
+  {
+    "revision": "84ca485f6fb5f1978a9f",
+    "url": "/robofriends/static/js/2.12e19136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.12e19136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76f82c27bebbda439461",
+    "url": "/robofriends/static/js/main.0fe5fa75.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
